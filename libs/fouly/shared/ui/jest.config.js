949620a1a97aa8657b218f254b4b164ebb257e93
Mathieu <mathieu.guilbault@gmail.com>
2020-05-11 11:57:48 -0400

    move shared-ui to the right place

--- libs/fouly/shared/ui/jest.config.js
+++ libs/fouly/shared/ui/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  name: 'fouly-ui',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/libs/fouly/ui',
+  name: 'fouly-shared-ui',
+  preset: '../../../../jest.config.js',
+  coverageDirectory: '../../../../coverage/libs/fouly/shared/ui',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
