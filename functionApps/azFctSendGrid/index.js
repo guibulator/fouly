a933945d143b0ff19c2f758fe7f6eaa5db0aa344
Mathieu Marcoux <matmarcoux@gmail.com>
2020-05-14 06:43:26 -0400

    set function app isolated project

--- functionApps/azFctSendGrid/index.js
+++ functionApps/azFctSendGrid/index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-module.exports = async function (context, req) {
+module.exports = async function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
   const apiKey = '';
@@ -10,13 +10,13 @@ module.exports = async function (context, req) {
   var message = {
     personalizations: [{ to: [{ email: 'contactskaremano@gmail.com' }] }],
     from: { email: 'matmarcoux@gmail.com' },
-    subject: 'Azure news',
+    subject: 'Feedback from user',
     content: [
       {
         type: 'text/plain',
-        value: 'testx',
-      },
-    ],
+        value: 'This is the user comments'
+      }
+    ]
   };
 
   const sendGridUrl = 'https://api.sendgrid.com/v3/mail/send';
@@ -25,14 +25,13 @@ module.exports = async function (context, req) {
     const { data } = await axios.post(sendGridUrl, message);
 
     context.res = {
-      // status: 200, /* Defaults to 200 */
-      body: 'Success',
+      body: 'Success'
     };
   } catch (err) {
     context.log(err);
     context.res = {
       status: 400,
-      body: 'Please pass a name on the query string or in the request body',
+      body: 'Please pass a name on the query string or in the request body'
     };
   }
 };
