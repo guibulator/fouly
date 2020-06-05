1d44e60976b4a3dd78a567afcbee67266d46857a
Mathieu <mathieu.guilbault@gmail.com>
2020-06-05 15:55:16 -0400

    use catchError instead to change strategy

--- libs/fouly/pwa/core/src/lib/providers/localisation-store.service.ts
+++ libs/fouly/pwa/core/src/lib/providers/localisation-store.service.ts
@@ -4,8 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 import { GeolocationResult } from '@skare/fouly/data';
-import { BehaviorSubject, from, onErrorResumeNext } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, from } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { ConfigService } from '../modules/config';
 @Injectable({ providedIn: 'root' })
 export class LocalisationStoreService {
@@ -22,32 +22,33 @@ export class LocalisationStoreService {
   // from device.. The Google geolocation api needs some infos from device and towers to approximate this. For now
   // we use ipstack
   initPosition() {
-    return onErrorResumeNext(
-      from(
-        this.geolocation
-          .getCurrentPosition()
-          .then((value) => {
-            this._currentPosition$.next(value);
-          })
-          .catch((error) => {
-            console.error('Error getting location', error);
-            throw error;
-          })
-      ),
-      this.httpClient.get<GeolocationResult>(`${this.configService.apiUrl}/geo`).pipe(
-        tap((result) =>
-          this._currentPosition$.next({
-            coords: {
-              latitude: result.lat,
-              longitude: result.lng,
-              accuracy: 20000,
-              altitude: 0,
-              altitudeAccuracy: 1000,
-              heading: 0,
-              speed: 0
-            },
-            timestamp: Date.now()
-          })
+    return from(
+      this.geolocation
+        .getCurrentPosition()
+        .then((value) => {
+          this._currentPosition$.next(value);
+        })
+        .catch((error) => {
+          console.error('Error getting location', error);
+          throw error;
+        })
+    ).pipe(
+      catchError(() =>
+        this.httpClient.get<GeolocationResult>(`${this.configService.apiUrl}/geo`).pipe(
+          tap((result) =>
+            this._currentPosition$.next({
+              coords: {
+                latitude: result.lat,
+                longitude: result.lng,
+                accuracy: 20000,
+                altitude: 0,
+                altitudeAccuracy: 1000,
+                heading: 0,
+                speed: 0
+              },
+              timestamp: Date.now()
+            })
+          )
         )
       )
     );
