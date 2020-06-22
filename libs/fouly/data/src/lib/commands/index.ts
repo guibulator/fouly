e812ba8e3502fd5442897147761be011520bfa3a
Mathieu Marcoux <matmarcoux@gmail.com>
2020-06-22 10:33:38 -0400

    fix build

--- libs/fouly/data/src/lib/commands/index.ts
+++ libs/fouly/data/src/lib/commands/index.ts
@@ -1,4 +1,5 @@
 export * from './chat-message.command';
+export * from './contribute.command';
 export * from './send-mail.command';
 export * from './store-crowd.command';
 export * from './user.command';
