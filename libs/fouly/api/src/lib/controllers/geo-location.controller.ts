be7b6280ee179b9d96a7f865b574f5bcd2a93df3
Mathieu <mathieu.guilbault@gmail.com>
2020-06-25 17:28:19 -0400

    fix when ip doesnt exist in some cases

--- libs/fouly/api/src/lib/controllers/geo-location.controller.ts
+++ libs/fouly/api/src/lib/controllers/geo-location.controller.ts
@@ -1,21 +1,29 @@
-import { Controller, Get, Ip } from '@nestjs/common';
+import { Controller, Get, Logger, Request } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GeolocationResult } from '@skare/fouly/data';
 import axios from 'axios';
 @Controller('geo')
 export class GeoLocationController {
   private ipStackKey = 'FOULY-IPSTACK-API-KEY'; //1. free -> 10k request/month with http only 2. 10$/Month -> 50k with https
-  constructor(private configurationService: ConfigService) {}
+  constructor(private configurationService: ConfigService, private logger: Logger) {}
   @Get()
-  async getData(@Ip() ip): Promise<GeolocationResult> {
+  async getData(@Request() request): Promise<GeolocationResult> {
+    const ip = request.ip;
+    this.logger.verbose(`Trying to get geolocation from ip address ${ip} `);
     const result = await axios.get(
       `http://api.ipstack.com/${ip}?access_key=${this.configurationService.get<string>(
         this.ipStackKey
       )}`
     );
-    console.log(
-      `Getting geolocation from ip:${ip} -> lat:${result.data.latitude} lng:${result.data.longitude} `
-    );
+    if (!result?.data?.latitude) {
+      this.logger.log('Could not get location from ip address, using Montreal as default.');
+      // If geolocation failed by ip, we return for lat position of Montreal
+      return { lat: 45.4993445, lng: -73.5709779 };
+    } else {
+      this.logger.log(
+        `Got geolocation from ip:${ip} -> lat:${result.data.latitude} lng:${result.data.longitude} `
+      );
+    }
     return { lat: result.data.latitude, lng: result.data.longitude };
   }
 }
