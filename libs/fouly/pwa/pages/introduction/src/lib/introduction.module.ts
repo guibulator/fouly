eba5e720168359f83b251119d8881bc1ac81a7bc
Frédérick Roy <kane78@hotmail.com>
2020-07-03 01:34:48 +0000

    Merged PR 49: translate Introduction
    Ajout traduction suivant texte anglais et francais revise
    
    Pour info @<arnaud.jean@gmail.com>
    
    Related work items: #44

--- libs/fouly/pwa/pages/introduction/src/lib/introduction.module.ts
+++ libs/fouly/pwa/pages/introduction/src/lib/introduction.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
+import { TranslateModule } from '@ngx-translate/core';
 import { FoulyUiModule } from '@skare/fouly/shared/ui';
 import { IntroductionRoutingModule } from './introduction-routing.module';
 import { IntroductionComponentPage } from './introduction/introduction.component';
@@ -11,6 +12,7 @@ import { IntroductionComponentPage } from './introduction/introduction.component
     IonicModule,
     IonicStorageModule,
     IntroductionRoutingModule,
+    TranslateModule,
     FoulyUiModule
   ],
   declarations: [IntroductionComponentPage]
