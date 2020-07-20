c0556574b4e9064667a1fe1f29fe8a596dd47c15
Mathieu <mathieu.guilbault@gmail.com>
2020-07-20 00:52:50 -0400

    Fix redirection issue and map destroy(glitch with redrawing the map is fixed)

--- libs/fouly/pwa/pages/login/src/lib/guards/redirect-to-profile.guard.ts
+++ libs/fouly/pwa/pages/login/src/lib/guards/redirect-to-profile.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthenticationService } from '@skare/fouly/pwa/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -8,10 +8,14 @@ import { map } from 'rxjs/operators';
 })
 export class RedirectToProfileIfLoggedInGuard implements CanActivate {
   constructor(private router: Router, private readonly authService: AuthenticationService) {}
-  canActivate(): Observable<boolean | UrlTree> {
+  canActivate(_, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+    // if we come from login page and user is logged in, redirect to the root
     const result$ = this.authService.currentUser$.pipe(
       map((user) => {
-        if (user) return this.router.parseUrl('/identity/profile');
+        if (user) {
+          if (state.url === '/identity/login') return this.router.parseUrl('/');
+          return this.router.parseUrl('/identity/profile');
+        }
         return true;
       })
     );
