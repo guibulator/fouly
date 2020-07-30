a47ea7c3b14cb1e6e69759977ee75da5747f25f9
Mathieu <mathieu.guilbault@gmail.com>
2020-07-29 20:10:28 -0400

    do not wait for fav to load but use number of saved fav instead for instant load and skeleton display
    also put a todo that throws an exception and prevent app from working

--- libs/fouly/pwa/app-shell/src/lib/guards/redirect-to-my-place.guard.ts
+++ libs/fouly/pwa/app-shell/src/lib/guards/redirect-to-my-place.guard.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { FavoriteResult } from '@skare/fouly/data';
-import { FavoriteStoreService, UserPreferenceService } from '@skare/fouly/pwa/core';
-import { Observable, of, race } from 'rxjs';
-import { filter, map, mapTo } from 'rxjs/operators';
+import { UserPreferenceService } from '@skare/fouly/pwa/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 /**
  * If this is the first time a user access the site, don't bother waiting for the favorite to initialize.
  * After 3 favorites, we show the favorite place at first. It contains
@@ -15,27 +14,20 @@ import { filter, map, mapTo } from 'rxjs/operators';
 export class RedirectToMyPlaceGuard implements CanActivate {
   private readonly REDIRECT_TO_FAV_PAGE_LIMIT = 3;
   private hasBeenTested = false;
-  constructor(
-    private router: Router,
-    private favoriteStoreService: FavoriteStoreService,
-    private userPrefService: UserPreferenceService
-  ) {}
+  constructor(private router: Router, private userPrefService: UserPreferenceService) {}
   canActivate(_, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
     if (this.hasBeenTested) return of(true);
-    const isFirstTimeUser$ = this.userPrefService.store$.pipe(
-      filter(({ firstTimeUser }) => firstTimeUser),
-      mapTo(true)
-    );
+
     // Whichever completes first will decide the faith of the route
     // if isFirstTimeUser then return true otherwise wait for the favorites
-    return race(isFirstTimeUser$, this.favoriteStoreService.store$).pipe(
-      map((data) => {
-        if (typeof data === 'boolean') {
+    return this.userPrefService.store$.pipe(
+      map(({ firstTimeUser, numberOfFavorites }) => {
+        if (firstTimeUser) {
           this.hasBeenTested = true;
           this.userPrefService.setFirstTimeUser(false);
           return true;
         }
-        if ((data as FavoriteResult[]).length > this.REDIRECT_TO_FAV_PAGE_LIMIT) {
+        if (numberOfFavorites > this.REDIRECT_TO_FAV_PAGE_LIMIT) {
           return this.router.parseUrl('/app/tabs/my-places');
         }
         return true;
